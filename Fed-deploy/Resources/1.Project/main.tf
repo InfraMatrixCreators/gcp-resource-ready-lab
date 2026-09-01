db28b2c248295818dfeb897538c0a84c28c718b0
Jitendar88 <jitendarsingh04@gmail.com>
2026-09-01 22:31:28 +0530

    changes done

--- Fed-deploy/Resources/1.Project/main.tf
+++ Fed-deploy/Resources/1.Project/main.tf
@@ -1,9 +1,9 @@
-resource "google_project" "stg_project" {
-    for_each = var.stg_projects
-    name = each.value.project_name
-    project_id = each.value.project_id
-    org_id = each.value.organization_id
-    billing_account = each.value.billing_account
-    deletion_policy = "DELETE"
+# resource "google_project" "stg_project" {
+#     for_each = var.stg_projects
+#     name = each.value.project_name
+#     project_id = each.value.project_id
+#     org_id = each.value.organization_id
+#     billing_account = each.value.billing_account
+    
 
-}
+# }
