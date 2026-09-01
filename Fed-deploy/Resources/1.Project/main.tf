5aa4f7a3efffde0fe7dfe76b060cf31f1bc268f8
Jitendar88 <jitendarsingh04@gmail.com>
2026-09-01 22:36:53 +0530

    changes done

--- Fed-deploy/Resources/1.Project/main.tf
+++ Fed-deploy/Resources/1.Project/main.tf
@@ -1,9 +1,10 @@
-# resource "google_project" "stg_project" {
-#     for_each = var.stg_projects
-#     name = each.value.project_name
-#     project_id = each.value.project_id
-#     org_id = each.value.organization_id
-#     billing_account = each.value.billing_account
-    
+resource "google_project" "stg_project" {
+    for_each = var.stg_projects
+    name = each.value.project_name
+    project_id = each.value.project_id
+    org_id = each.value.organization_id
+    billing_account = each.value.billing_account
+    deletion_policy = "DELETE"
 
-# }
+
+}
